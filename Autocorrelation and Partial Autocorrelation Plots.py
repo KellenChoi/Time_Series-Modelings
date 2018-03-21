239d74671e39b1fd51732cfeb1878edc7c14be37
kellen0128 <kellen0128@gmail.com>
2018-03-21 11:26:15 -0400

    Create Autocorrelation and Partial Autocorrelation Plots.py

---
+++ Autocorrelation and Partial Autocorrelation Plots.py
@@ -0,0 +1,4 @@
+##### Autocorrelation plot(Correlogram): correlation of the series with itself, lagged by x time units
+######## great info for indentifying and interpreting ACF and PACF 
+
+from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
