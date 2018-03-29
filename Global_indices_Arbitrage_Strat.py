50be03a573650b33e811467801757549a8ae0766
kellen0128 <kellen0128@gmail.com>
2018-03-29 07:59:24 -0400

    Create Global_indices_Arbitrage_Strat.py

---
+++ Global_indices_Arbitrage_Strat.py
@@ -0,0 +1,4 @@
+####### To use information from an earlier time zone to your advantage in a later time zone.
+##### Index Country Closing Time (EST) Hours Before S&P Close All Ords Australia 0100 15 Nikkei 225 Japan 0200 14 
+##### Hang Seng Hong Kong 0400 12 DAX Germany 1130 4.5 FTSE 100 UK 1130 4.5 NYSE Composite US 1600 0 
+##### Dow Jones Industrial Average US 1600 0 S&P 500 US 1600 0
